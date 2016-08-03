de0437b53d88f8e8ab70ba3642d7a0abf984cad0
andrew <twinklechopper@gmail.com>
2016-08-03 18:04:24 +0300

    Users router and schema completed

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	name: String
+	var userSchema = new Schema({
+    globalId: Schema.Types.ObjectId,
+    login: String,
+    userName: String,
+    userSurname: String,
+    avatar: Buffer,
+    rights: [String]
 });
 
 module.exports = mongoose.model('User', userSchema); 
