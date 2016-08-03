3d282bf4b8fd7cba2af0afbe346369903f451804
IliaVolk <liailia119@gmail.com>
2016-08-04 01:09:43 +0300

    Database: modify section schema

--- backend/schemas/sectionSchema.js
+++ backend/schemas/sectionSchema.js
@@ -1,11 +1,10 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var ObjectId = mongoose.Types.ObjectId;
 
 var sectionSchema = new Schema({
-    //ObjectId will be generated automatically
     name: String,
-    description: String
+    description: String,
+    features: {type: Schema.Types.ObjectId, ref:"Feature"}
 });
 module.exports = mongoose.model('Section', sectionSchema);
