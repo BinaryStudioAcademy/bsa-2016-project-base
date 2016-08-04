4a16f388d0854103c938d769465dcb6de1c5f150
Oleh Soseniuk <oleh.soseniuk@gmail.com>
2016-08-04 10:30:57 +0300

    Add react-bootstrap and react-router-bootstrap

--- frontend/components/home/Home.js
+++ frontend/components/home/Home.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import styles from './home.sass';
+import { Button } from 'react-bootstrap';
 
 class Home extends Component {
 	constructor(props) {
 	    super(props);
+	    this.onClick = this.onClick.bind(this);
 	}
+
+	onClick() {
+		console.log('Click');
+	}
+
  	render() {
 	    return (
-	    	<div className={styles.alert}>Welcome Home!</div>
+	    	<div>
+	    		<div className={styles.alert}>Welcome Home!</div>
+	    	  <Button bsStyle="primary" bsSize="large" onClick={this.onClick}>OK</Button>
+	    	</div>
+	    	
 	    )
 	}
 };
