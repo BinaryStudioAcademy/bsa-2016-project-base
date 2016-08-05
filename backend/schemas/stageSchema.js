95f59fae9fe2a1ad7bd880706a05d76729e1a9da
D-aVi-D <davids_mbox@bigmir.net>
2016-08-05 13:58:13 +0300

    Updated DB and 'stageSchema.js'

--- backend/schemas/stageSchema.js
+++ backend/schemas/stageSchema.js
@@ -2,8 +2,11 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var StageSchema = new Schema({
+
 	stageName: String,
-  commisioned: {type: Date},
-  decommisioned: {type: Date}
+ 	commisioned: {type: Date},
+ 	decommisioned: {type: Date}
+ 	
 })
+
 module.exports = mongoose.model('Stage', StageSchema);
