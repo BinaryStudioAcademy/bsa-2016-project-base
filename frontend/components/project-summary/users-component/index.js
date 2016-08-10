1fe9577c22e3303747cac6425b4b64d5469ef25d
andrew <twinklechopper@gmail.com>
2016-08-10 11:14:12 +0300

    remove redux promise

--- frontend/components/project-summary/users-component/index.js
+++ frontend/components/project-summary/users-component/index.js
@@ -3,12 +3,10 @@ import UserList from './users_list';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './reducers/root_reducer';
-import ReduxPromise from 'redux-promise';
 import thunk from 'redux-thunk';
-// console.log(rootReducer);
+
 const middleware = applyMiddleware(thunk);
 const store = createStore(rootReducer, middleware);
-// const store = createStoreWithMiddleware(rootReducer);
 export default class User extends Component {
     render() {
         return (
