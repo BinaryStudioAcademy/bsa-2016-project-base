bf615609a7269a88810b0ea683d77b4a720d51b0
Oleh Soseniuk <oleh.soseniuk@gmail.com>
2016-08-10 10:27:28 +0300

    Change navbar styling

--- frontend/components/navbar/Navbar.js
+++ frontend/components/navbar/Navbar.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Grid, Row, Panel, Nav,NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import styles from './navbar.sass';
 
 class Navbar extends Component {
 	constructor(props) {
 	    super(props);
+         this.state = {
+           isMenuActive: false
+        };
+        this.toggleMenu = this.toggleMenu.bind(this);
 	}
+    
+    toggleMenu(e) {
+        let state = this.state.isMenuActive;
+        this.setState({
+            isMenuActive: !state
+        });
+    }
  	render() {
         return (
-            <div className={styles.menu}>
-                <div className={styles.menuItem}>
-                    <Link to={"/projects"} >Projects</Link>
+            <div className="nav-home">
+                <div className={styles["toggle-menu"]} onClick={this.toggleMenu}> 
+                    <i className={"fa fa-2x " + (this.state.isMenuActive ? "fa-times" : "fa-bars" )} aria-hidden="true"></i>
                 </div>
-                <div className={styles.menuItem}>
-                    <Link to={"/stats"} >Stats</Link>
-                </div>
-                <div className={styles.menuItem}>
-                    <Link to={"/review"} >Inquiry review</Link>
-                </div>
-                <div className={styles.menuItem}>
-                    <Link to={"/admin"} >Administration</Link>
-                </div>
+                <Nav bsStyle="pills" stacked  navbar className={styles["menu-bar"] +" "+(this.state.isMenuActive ? "menu-bar-visible" : "" )}>
+                    <LinkContainer to="/home" className={styles["menu-item"]}>
+                        <NavItem eventKey={1}>Home</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/review" className={styles["menu-item"]}>
+                        <NavItem eventKey={2}>Inquiry Review</NavItem>  
+                    </LinkContainer>  
+                    <LinkContainer to="/stats" className={styles["menu-item"]}>
+                        <NavItem eventKey={3}>Stats</NavItem>
+                    </LinkContainer> 
+                    <LinkContainer to="/admin" className={styles["menu-item"]}>
+                        <NavItem eventKey={4}>Admin Area</NavItem>
+                    </LinkContainer> 
+                </Nav>
             </div>
         )
     }
