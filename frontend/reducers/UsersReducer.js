42f2a9a62b87922e60579980e6e07329b9085b1e
andrew <twinklechopper@gmail.com>
2016-08-10 21:28:13 +0300

    fixed reducer bug

---
+++ frontend/reducers/UsersReducer.js
@@ -0,0 +1,32 @@
+import * as types from '../constants/UsersActionTypes';
+
+const INITIAL_STATE = {
+    users: []
+};
+
+
+export default function UsersReducer(state = INITIAL_STATE, action) {
+    switch(action.type){
+        case types.USERS_GET_ALL_START_LOADING: {
+            return Object.assign({}, state, {
+                isLoading: true
+            });
+        }
+        case types.USERS_GET_ALL_LOADED: {
+            return Object.assign({}, state, {
+                isLoading: false,
+                users: action.payload
+            });
+        }    
+        case types.USERS_GET_ALL_ERROR: {
+            console.log(action.error);
+            return Object.assign({}, state, {
+                isLoading: false,
+                error: action.error
+            });
+        }    
+        default: {
+            return state;
+        }
+    }
+};
