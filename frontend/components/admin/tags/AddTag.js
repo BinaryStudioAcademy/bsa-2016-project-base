698c416951e42378c3bd3049404bc28176dcceca
Oleh Soseniuk <oleh.soseniuk@gmail.com>
2016-08-10 10:28:23 +0300

    Change Tags component markup and styles

--- frontend/components/admin/tags/AddTag.js
+++ frontend/components/admin/tags/AddTag.js
@@ -4,7 +4,7 @@ import styles from './tags.sass';
 
 const AddTag = ({setTagName, addTag}) => {
   	return (
-		<Col xs={12} sm={4} md={4}>
+		<Col xs={12} sm={12} md={4}>
             <div className={styles["tags-creation"]}>
             <FormGroup>
                 <InputGroup className={styles["tags-input-group"]}>
