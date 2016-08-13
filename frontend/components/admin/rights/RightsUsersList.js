aefac65ec0c6fbbd454a3b8d50c7f8390bc3d064
KuriatoV <kuriato.valentyn@gmail.com>
2016-08-13 21:57:09 +0300

    some styles

---
+++ frontend/components/admin/rights/RightsUsersList.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+import {Grid, FormControl, Row, Col, Button,Checkbox} from 'react-bootstrap';
+import styles from './styles/Rights.sass';
+
+class RightsUsersList extends Component {
+	constructor(props) {
+	    super(props);
+	}
+ 	render() {
+	    return (
+	    	<div className=" rightsUsersList">
+          LIST OF USERS INSIDE ME
+      
+
+	    	</div>
+	    )
+	}
+};
+
+
+
+
+export default RightsUsersList;
