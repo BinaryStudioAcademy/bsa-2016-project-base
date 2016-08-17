cf42e26d81e5c4d18e31e09c4ad4b73f1428f4f1
IliaVolk <liailia119@gmail.com>
2016-08-18 02:18:59 +0300

    added forgotten file

--- backend/service/documents/documentService.js
+++ backend/service/documents/documentService.js
@@ -3,7 +3,6 @@
  */
 var google = require('googleapis');
 var fs = require("fs");
-var open = require('open');
 var Docxtemplater = require('docxtemplater');
 var path = require("path");
 var NeedAuthError = require("./NeedAuthError");
@@ -16,19 +15,17 @@ class DocumentService {
         this.scopes = [
             'https://www.googleapis.com/auth/drive'
         ];
-
     }
 
-    authentication(callback) {
+    authentication(callback){
         var url = this.oauth2Client.generateAuthUrl({
             access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
             scope: this.scopes // If you only need one scope you can pass it as string
         });
-        callback && callback(null, url);
-        //open(url);//open auth url
+        callback(null, url);
     }
 
-    getDocument(query, callback) {
+    getDocument(query, tokens, callback) {
         fs.readFile(path.join("upload/resources/documents/templates/testTemplate.docx"), "binary", function (err, content) {
             var doc = new Docxtemplater(content);
             //set the templateVariables
@@ -46,55 +43,29 @@ class DocumentService {
 
             var buf = doc.getZip()
                 .generate({type: "nodebuffer"});
-            this.uploadFile(buf, callback)
+            this.uploadFile(buf, tokens, callback)
         }.bind(this))
     }
 
-    getAccessToken(callback) {
-        //return token from cookie
-        /*
-         if (no token){
-         notify about requirement of auth
-         }
-         */
-        if (!this.accessToken) {
-            callback(new NeedAuthError())
-        }
-        callback(null, this.accessToken)
-    }
-
-    setAccessToken(token) {
-        //set token in cookie
-        this.accessToken = token;
-    }
-
-    uploadFile(file, callback) {
-        this.getAccessToken(function(err, token){
+    uploadFile(file, tokens, callback) {
+        this.oauth2Client.setCredentials({access_token:tokens.access_token});
+        this.drive.files.create({
+            resource: {
+                name: "upload" + Date.now(),
+                mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            },
+            fields: 'webViewLink,id',
+            media: {
+                mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                body: file // read streams are awesome!
+            }
+        }, function (err, file, res) {
             if (err) callback(err);
             else {
-                this.oauth2Client.setCredentials({
-                    access_token: token
-                });
-                this.drive.files.create({
-                    resource: {
-                        name: "upload" + Date.now(),
-                        mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                    },
-                    fields: 'webViewLink',
-                    media: {
-                        mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                        body: file // read streams are awesome!
-                    }
-                }, function (err, file, res) {
-                    if (err) callback(err);
-                    else{
-                        console.log("Uploaded ", file.webViewLink);
-                        callback(err, file.webViewLink)
-                    }
-
-                });
+                console.log("Uploaded ", file.webViewLink);
+                callback(err, file.webViewLink, file.id)
             }
-        }.bind(this))
+        });
     }
 
     redirectUrlCallback(req, res) {
@@ -103,7 +74,8 @@ class DocumentService {
             if (error) {
                 res.err = error
             }
-            this.setAccessToken(tokens.access_token);
+            var tokensStr = JSON.stringify(tokens);
+            res.redirect(`/?tokens=${tokensStr}`);
         }.bind(this));
     }
 }
