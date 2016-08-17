c69370c5a910cfabbbfc8b95c440570c64ce5168
D-aVi-D <davids_mbox@bigmir.net>
2016-08-17 12:35:07 +0300

    Updated FeatureSchema and DB.

--- backend/schemas/featureSchema.js
+++ backend/schemas/featureSchema.js
@@ -2,31 +2,45 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Feature = new Schema({
-
 	featureName: String,
-
 	section: {type: Schema.Types.ObjectId, ref: 'Section'},
-
 	featureOrder: String,
-
 	isNecessary: Boolean,
-
-	featureDescription: {
-		images: [{shortName: String, binBody: Buffer}],
-		extImagesLinks: [String],
-		attachments: [{fileName: String, binBody: Buffer}],
-		extLinks: [String],
-		lists: [String]										// Changed 04/08/16   [[String]]
-	},
-
+	descriptionText: String,
+	descriptionHTMLText:String,
 	created: Date,
-
 	isImplemented: Boolean,
-
 	childFeatures: [{type: Schema.Types.ObjectId, ref: 'Feature'}],
-
 	isItSubFeature: Boolean
 });
+
+
+// var Feature = new Schema({					// Commented 17/08/2016
+
+// 	featureName: String,
+
+// 	section: {type: Schema.Types.ObjectId, ref: 'Section'},
+
+// 	featureOrder: String,
+
+// 	isNecessary: Boolean,
+
+// 	featureDescription: {
+// 		images: [{shortName: String, binBody: Buffer}],
+// 		extImagesLinks: [String],
+// 		attachments: [{fileName: String, binBody: Buffer}],
+// 		extLinks: [String],
+// 		lists: [String]										// Changed 04/08/16   [[String]]
+// 	},
+
+// 	created: Date,
+
+// 	isImplemented: Boolean,
+
+// 	childFeatures: [{type: Schema.Types.ObjectId, ref: 'Feature'}],
+
+// 	isItSubFeature: Boolean
+// });
 
 // var featureSchema = new Schema({
 // 	featureName: String,
