7622ed08436c7e4bbb3747b6fa6daeb4c3fcca9a
Anastacia Batarey <batarey.work@gmail.com>
2016-08-19 22:14:56 +0300

    Use material-ui: DatePickerUI

--- frontend/components/common/DatePickerUI.js
+++ frontend/components/common/DatePickerUI.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 /**
  * `DatePicker` can be implemented as a controlled input,
  * where `value` is handled by state in the parent component.
  */
-export default class DatePickerExampleControlled extends React.Component {
+export default class DatePickerControlled extends React.Component {
 
   constructor(props) {
     super(props);
@@ -23,11 +24,13 @@ export default class DatePickerExampleControlled extends React.Component {
 
   render() {
     return (
-      <DatePicker
-        hintText="Controlled Date Input"
-        value={this.state.controlledDate}
-        onChange={this.handleChange}
-      />
+      <MuiThemeProvider>
+        <DatePicker
+          hintText="Controlled Date Input"
+          value={this.state.controlledDate}
+          onChange={this.handleChange}
+        />
+      </MuiThemeProvider>
     );
   }
 }
