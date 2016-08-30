0172f9d794ac5cb2023b14e0b10020d8f15c0752
Oleh Soseniuk <oleh.soseniuk@gmail.com>
2016-08-30 14:42:36 +0300

    Add new props to FileUpload

--- frontend/components/common/FileUpload.js
+++ frontend/components/common/FileUpload.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 
-const FileUpload = ({id, accept,  onChange}) => {
+const FileUpload = ({id, accept, multiple, onChange}) => {
     return (
         <div>
-            <input id={id} type="file" accept={accept} onChange={onChange}/>
+            <input id={id} type="file" accept={accept} onChange={onChange} multiple={multiple}/>
         </div>
     );
 }
@@ -11,7 +11,12 @@ const FileUpload = ({id, accept,  onChange}) => {
 FileUpload.propTypes = {
     id: PropTypes.string,
 	accept: PropTypes.string,
+	multiple: PropTypes.bool,
     onChange: PropTypes.func
+};
+
+FileUpload.defaultProps = {
+	multiple: true
 };
 
 export default FileUpload;
