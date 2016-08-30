0706c9748bb0072699f14442820926440e77cfaf
KuriatoV <kuriato.valentyn@gmail.com>
2016-08-30 16:53:36 +0300

    Resizable graphs

--- frontend/components/stats/ChartWrapper.js
+++ frontend/components/stats/ChartWrapper.js
@@ -1,20 +1,16 @@
 import React from "react"
-import {PropTypes} from "react"
 import styles from "./stats.sass"
+import ResizableBox from "react-resizable-box"
 export default class ChartWrapper extends React.Component {
-    constructor() {
-        super()
-    }
-
-    static get propTypes() {
-        return {}
-    }
-
     render() {
-        return <div className={styles.chart}>
-			<h2> {this.props.title} </h2>
-            {this.props.children}
-            </div>
+
+        return <ResizableBox>
+                <div className={styles.chart}>
+                    <h2> {this.props.title} </h2>
+                    </br>
+                    {this.props.children}
+                </div>
+            </ResizableBox>
+
     }
 }
-
