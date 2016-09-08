b0cfe9c5bcb84ee45f52e2ac51e039710a7353bd
OksanaMilevska <oksanarybiy@ukr.net>
2016-09-08 14:37:39 +0300

    Update init.js

--- frontend/test/searchTest/init.js
+++ frontend/test/searchTest/init.js
@@ -1,4 +1,4 @@
-describe("users should be able to login and use simple Search", function() {
+describe("users should be able to login", function() {
     it("to fill in user information and login", function () {
          //"to fill in user information and login",
          browser.get("http://localhost:2020/#/");
@@ -9,7 +9,7 @@ describe("users should be able to login and use simple Search", function() {
          //press Login button
          var loginButton = element(by.cssContainingText(".btn", "Log in"));
          loginButton.click()
-         //console.log("Hello");
+        
         //browser.get("http://localhost:6500/");
     })
 })
