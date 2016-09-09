5809a85647755f30e85a80e217b04adf74ebc696
andrew <twinklechopper@gmail.com>
2016-09-09 09:57:08 +0300

    restyled poroject list

--- frontend/components/home/components/Project.js
+++ frontend/components/home/components/Project.js
@@ -3,17 +3,32 @@ import { Link } from 'react-router';
 import { ListGroupItem, Label } from 'react-bootstrap';
 
 import styles from './Project.sass'
-import {FaCheckCircleO, FaCogs} from 'react-icons/lib/fa';
+import {FaCheckCircleO, FaCogs, FaTh} from 'react-icons/lib/fa';
 export default class Project extends Component {
 
     static propTypes = {
         project: React.PropTypes.object.isRequired,
-        ranking: React.PropTypes.string.isRequired
+        ranking: React.PropTypes.number.isRequired
     };
 
+    createLis() {
+        let lis = [];
+       
+        for (let i = 0; i < 5; i++) {
+            console.log(this.props.ranking - 1);
+            if (i <= this.props.ranking - 1) {
+                lis.push(<li className={styles.ranked}></li>);
+            }
+            else {
+                lis.push(<li className={styles.unranked}></li>); 
+            }
+        }
+        return lis;
+    }
     render() {
 
         const { project, ranking, id} = this.props;
+        let lis = this.createLis();
         console.log(project)
         return (
             <li key={id} className={styles['list-group-item']}>
@@ -23,41 +38,47 @@ export default class Project extends Component {
                             {project.projectName}
                         </h4>
                         {
-                            (project.status === "Completed") ?
-                                <div className={styles["status-completed"]}><div className={styles["status-completed-label"]}>Completed</div></div> :
-                                <div className={styles["status-inprogress"]}><div className={styles["status-inprogress-label"]}>In Progress</div></div>
+                        (project.status === "Completed") ?
+                            <div className={styles["status-completed"]}><div className={styles["status-completed-label"]}>Completed</div></div> :
+                            <div className={styles["status-inprogress"]}><div className={styles["status-inprogress-label"]}>In Progress</div></div>
                         }
-                        
                     </div>
                     <div className={styles.image}>
-                <img src="http://placehold.it/140x100" />
-                </div>
-                <div className={styles['labels-container']}>
-                <div className={styles.labels} >
-                    <span className={styles['rank-label']}>ranking</span>
-                    <span className={styles['label-rank-number']} >{ranking}/5.0</span>
-                </div>
+                        <img src="http://placehold.it/140x100" />
+                    </div>
+                    <div className={styles['description-t']}>
+                        {project.description.descrText} 
+                    </div>
+                    <div className={styles['ranking-row']}>
+                        <div className={styles.rating} >
+                            <span className={styles.name}>Ranking:</span>
+                            <ul className={styles["ranking-list"]}>
+                              {lis}
+                            </ul>
+                            
+                            {/*<span className={styles['number']} >{ranking}/5.0</span>*/}
+                        </div>
 
-                {(project.technologies) ?
-                    <div className={styles.labels}>
-                        {project.technologies.map((tech, index) => {
-                            return (
-                                <span key={index} className={styles['tech-label']}>
-                                    <span className={styles['tech-name']}>{tech.techName}</span>
-                                    <span className={styles['label-version']} >{tech.techVersion || "1.0"}</span>
-                                </span>
-                            );
-                        })}
-                    </div> : null
-                }
-                </div>
-                <div className={styles.description} >
-                    {project.description.descrText}
-                </div>
-                {(project.timeEnd)?
-                    <div className={styles['update-date']} >Project finished on: {project.timeEnd.split('T')[0]}</div>:
-                    <div>Progect in process</div>
-                }
+                        {(project.technologies) ?
+                            <div className={styles.technoligies}>
+                            <div>
+                          
+                                    <FaTh size={12}/>
+                                 </div>   
+                       
+                                <div className={styles.name}>{project.technologies.length}</div>
+                                <ul className={styles["tech-list"]}>
+                                    {project.technologies.map((tech, index) => {
+                                        return (
+                                            <li key={index} className={styles['tech-label']}>
+                                                <span className={styles['tech-name']}>{tech.techName}</span>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div> : null
+                        }
+                    </div>
                 </Link>
             </li>
 
