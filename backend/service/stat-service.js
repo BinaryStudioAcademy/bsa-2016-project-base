ccd0ae8b022e5c65f7a5db684e687380fafc429a
D-aVi-D <davids_mbox@bigmir.net>
2016-09-11 10:18:33 +0300

    Some data to charts linked. Other in work.

--- backend/service/stat-service.js
+++ backend/service/stat-service.js
@@ -49,7 +49,20 @@ class StatService {
 		
 	}
 
-	
+	getProjectsDatesStart(req, callback){
+		Projects.aggregate([{$project: {projectName: 1, timeBegin: 1, timeEnd: 1}}, {$sort: {timeBegin: 1}}])
+			.exec((err, result)=>{
+				let datesBounds = {};
+        		let timeOffset = new Date().getTimezoneOffset();
+				let startMonth = new Date(result[0].timeBegin.getFullYear(), result[0].timeBegin.getMonth(), 1, 0, 0 - timeOffset, 0, 0);
+				//console.log('startMonth: ', startMonth);
+				let endMonth = new Date(result[result.length - 1].timeBegin.getFullYear(), result[result.length - 1].timeBegin.getMonth() + 1, 1, 0, 0 - timeOffset, 0, 0);
+				datesBounds.startMonth = startMonth;
+				datesBounds.endMonth = endMonth;
+				datesBounds.projList = result;
+				callback(null, datesBounds);
+			});
+	}
 }
 
 const statService = new StatService();
