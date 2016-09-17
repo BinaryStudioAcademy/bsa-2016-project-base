6eec6450164fc83b0ac720e5db04de25117ac87e
IliaVolk <liailia119@gmail.com>
2016-09-17 17:59:04 +0300

    fixed some image url

--- frontend/constants/Api.js
+++ frontend/constants/Api.js
@@ -2,8 +2,8 @@ export const HOST = 'localhost';
 export const PROTOCOL = 'http';
 export const PORT = '6500';
 export const URL = `/projects/api/`;
-export const DEFAULT = `http://team.binary-studio.com/projects/resources/default/`;
 export const ORIGIN = `http://team.binary-studio.com/projects`;
+export const DEFAULT = `${ORIGIN}/resources/default/`;
 export const cookieMarker = { credentials: 'same-origin' };
 export const jsonHedeaders = {
     headers:{
