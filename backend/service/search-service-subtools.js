801d18479d23006cd9617222b44092296e2cedcb
D-aVi-D <davids_mbox@bigmir.net>
2016-09-19 22:36:47 +0300

    Fix back the predicates location search for another decision.

--- backend/service/search-service-subtools.js
+++ backend/service/search-service-subtools.js
@@ -436,7 +436,7 @@ class SearchServiceSubTools {
                                     {selectionsConditionsAnd.namesIn.push(searchReturn.queryProjNames[parseInt(keyParts[2])])}
                                 else {selectionsConditionsAnd.techsNin.push(searchReturn.queryProjNames[parseInt(keyParts[2])])};
                                 break;
-                    case 'location':   if (elem.vars[elem_] == '1') 
+                    case 'id':   if (elem.vars[elem_] == '1') 
                                     {selectionsConditionsAnd.idsIn.push(searchReturn.queryIds[parseInt(keyParts[2])])}
                                 else {selectionsConditionsAnd.idsNin.push(searchReturn.queryIds[parseInt(keyParts[2])])};
                                 break;
