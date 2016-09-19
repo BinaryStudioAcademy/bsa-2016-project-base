3eecadceaee48a5292aad63f890d5ebc0b74863e
IliaVolk <liailia119@gmail.com>
2016-09-19 22:57:39 +0300

    merge develop

--- backend/service/search-service-subtools.js
+++ backend/service/search-service-subtools.js
@@ -436,6 +436,7 @@ class SearchServiceSubTools {
                                     {selectionsConditionsAnd.namesIn.push(searchReturn.queryProjNames[parseInt(keyParts[2])])}
                                 else {selectionsConditionsAnd.techsNin.push(searchReturn.queryProjNames[parseInt(keyParts[2])])};
                                 break;
+                    //should name location!!
                     case 'location':   if (elem.vars[elem_] == '1') 
                                     {selectionsConditionsAnd.idsIn.push(searchReturn.queryIds[parseInt(keyParts[2])])}
                                 else {selectionsConditionsAnd.idsNin.push(searchReturn.queryIds[parseInt(keyParts[2])])};
