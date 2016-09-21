6de6dfc9222c8be16afd0288be36b64f4dd9fa85
IliaVolk <liailia119@gmail.com>
2016-09-21 15:51:12 +0300

    url

--- frontend/constants/Api.js
+++ frontend/constants/Api.js
@@ -1,8 +1,8 @@
 export const HOST = 'localhost';
 export const PROTOCOL = 'http';
 export const PORT = '6500';
-export const URL = '/api/'
-//export const URL = `/projects/api/`;
+//export const URL = '/api/'
+export const URL = `/projects/api/`;
 export const ORIGIN = `http://team.binary-studio.com/projects`;
 export const DEFAULT = `${ORIGIN}/resources/default/`;
 export const cookieMarker = { credentials: 'same-origin' };
