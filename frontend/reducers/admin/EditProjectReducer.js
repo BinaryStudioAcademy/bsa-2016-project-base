2c15594b460b412b70ee62c5867e7679b3ba5f3a
Vladyslav Vystorobets <v.vystorobets@outlook.com>
2016-09-22 10:56:09 +0300

    fixed userStory (edit project)

--- frontend/reducers/admin/EditProjectReducer.js
+++ frontend/reducers/admin/EditProjectReducer.js
@@ -270,7 +270,7 @@ export default function EditProjectReducer(state = initialState, action) {
                                 //userStory[el._id].dateTo = history.dateTo;
                                 //userStory[el._id].projectId = history.projectId;
                             var b = {
-                                dateFrom: history.dateFrom,
+                                dateFrom: new Date(history.dateFrom),
                                 dateTo : history.dateTo,
                                 projectId : history.projectId,
                             }
@@ -289,7 +289,7 @@ export default function EditProjectReducer(state = initialState, action) {
                             //userStory[el._id].dateTo = history.dateTo;
                             //userStory[el._id].projectId = history.projectId;
                             var b = {
-                                dateFrom: history.dateFrom,
+                                dateFrom: new Date(history.dateFrom),
                                 dateTo : history.dateTo,
                                 projectId : history.projectId,
                             }
